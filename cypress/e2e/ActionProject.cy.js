42cd4e5c8bb41cb7c254a8d298fea8100ed5514e
emmytech07 <ilesanmiemmanuel07@gmail.com>
2023-01-17 16:43:05 +0100

    Updating project on to-do with action

--- cypress/e2e/ActionProject.cy.js
+++ cypress/e2e/ActionProject.cy.js
@@ -1,1 +1,22 @@
 /// <reference types="cypress" />
+
+context('Actions', () => {
+    beforeEach(() => {
+        cy.visit ('https://example.cypress.io/commands/actions')
+    })
+
+    it('Verify the text input on Gmail placeholder', ()=> {
+        cy.get("input[type='email']")
+          .type('fake@email.com', {delay: 300})
+          .should('have.value', 'fake@email.com')
+          .type('{del}{selectall}{backspace}')
+
+    
+
+    cy.get('.action-disabled')
+      // Ignore error checking prior to type
+      // like whether the input is visible or disabled
+      .type('disabled error checking', { force: true })
+      .should('have.value', 'disabled error checking')
+  })
+})
