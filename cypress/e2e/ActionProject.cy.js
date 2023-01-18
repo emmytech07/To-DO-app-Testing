97a88113bb2758ae3fbdcd41848b6ec99a21940a
emmytech07 <ilesanmiemmanuel07@gmail.com>
2023-01-18 05:57:30 +0100

    Updated with radio and checkbox uncheck verifications

--- cypress/e2e/ActionProject.cy.js
+++ cypress/e2e/ActionProject.cy.js
@@ -108,6 +108,21 @@ context('Actions', () => {
       cy.get('.action-radios [type="radio"]')
       .check('radio3', { force: true }).should('be.checked')
 
-  })
+  // .uncheck() accepts a value argument
+  cy.get('.action-check [type="checkbox"]')
+  .check('checkbox1')
+  .uncheck('checkbox1').should('not.be.checked')
+
+// .uncheck() accepts an array of values
+cy.get('.action-check [type="checkbox"]')
+  .check(['checkbox1', 'checkbox3'])
+  .uncheck(['checkbox1', 'checkbox3']).should('not.be.checked')
+
+// Ignore error checking prior to unchecking
+cy.get('.action-check [disabled]')
+  .uncheck({ force: true }).should('not.be.checked')
+})
+
+    })
 })
 
