43790b08dc4bbfe761593e181883f74a67ad144d
emmytech07 <ilesanmiemmanuel07@gmail.com>
2023-01-18 06:01:17 +0100

    Updated with radio and checkbox uncheck verificationss

--- cypress/e2e/ActionProject.cy.js
+++ cypress/e2e/ActionProject.cy.js
@@ -107,22 +107,22 @@ context('Actions', () => {
 
       cy.get('.action-radios [type="radio"]')
       .check('radio3', { force: true }).should('be.checked')
-
-  // .uncheck() accepts a value argument
-  cy.get('.action-check [type="checkbox"]')
-  .check('checkbox1')
-  .uncheck('checkbox1').should('not.be.checked')
-
-// .uncheck() accepts an array of values
+    
+//  verify .uncheck() accepts a value argument
 cy.get('.action-check [type="checkbox"]')
-  .check(['checkbox1', 'checkbox3'])
-  .uncheck(['checkbox1', 'checkbox3']).should('not.be.checked')
+.check('checkbox1')
+.uncheck('checkbox1').should('not.be.checked')
 
-// Ignore error checking prior to unchecking
+// verify .uncheck() accepts an array of values
+cy.get('.action-check [type="checkbox"]')
+.check(['checkbox1', 'checkbox3'])
+.uncheck(['checkbox1', 'checkbox3']).should('not.be.checked')
+
+// verify Ignore error checking prior to unchecking
 cy.get('.action-check [disabled]')
-  .uncheck({ force: true }).should('not.be.checked')
-})
-
+.uncheck({ force: true }).should('not.be.checked')
     })
+    
+
 })
 
